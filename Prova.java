2244513d0507d2bc16fef29b8bbd804b5ff35d61
LorenzoSilvestri <64147262+LorenzoSilvestri@users.noreply.github.com>
2020-04-24 15:07:50 +0200

    Add files via upload

---
+++ Prova.java
@@ -0,0 +1,10 @@
+
+public class Prova {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Prova");
+			System.out.println("Prova2");
+	}
+
+}
